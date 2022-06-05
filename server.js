df9ff880db95f665418e7467ef473c36af85095b
Achyuth Dev <achyuthdev@Achyuths-MacBook-Pro.local>
2022-06-05 18:11:03 +0530

    Server.js file completed

---
+++ server.js
@@ -0,0 +1,11 @@
+const express =  require("express");
+
+const app = express();
+
+app.get("/", function(request, response){
+    response.send("<h2>Hello</h2>");
+});
+
+app.listen(3000, function(){
+    console.log("Server started at port 3000");
+});
